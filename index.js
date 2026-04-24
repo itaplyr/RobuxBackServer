e1a386fb1bc9269cbf25da2debdcec51a8847f47
itaplyr <itaplyr@gmail.com>
2026-04-24 15:52:18 +0200

    V1.0 FIX

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import path from "path";
 
 const app = express()
 
-app.use(bodyParser.json({
-    verify: (req, res, buf) => {
-        req.rawBody = buf.toString();
+
+async function main() {
+    try {
+        app.use(bodyParser.json({
+            verify: (req, res, buf) => {
+                req.rawBody = buf.toString();
+            }
+        }));
+
+        app.get('/', (req, res) => {
+            return res.sendFile(path.join(process.cwd(), 'public', 'index.html'))
+        })
+
+        app.post('/newpurchase', (req, res) => {
+            return true
+        });
+
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`🌐 Webhook server running on port ${PORT}`);
+        });
+    } catch (e) {
+        console.log("Error: ", e)
     }
-}));
-
-app.get('/', (req, res) => {
-    return res.status(200)
-})
-
-app.post('/newpurchase', (req, res) => {
-    return true
-});
+}
+main()
